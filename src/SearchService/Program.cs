489cc508a284807391d6230a7d72233a4f82492a
vectorNull <jose.rodriguez76@protonmail.com>
2023-08-30 17:15:39 -0700

    Created DBInitializer to seed data if necessary; Seed data in json file

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,6 +1,4 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
-using SearchService.Models;
+using SearchService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +13,14 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
-
-await DB.Index<Item>()
-    .Key(x => x.Make, KeyType.Text)
-    .Key(x => x.Model, KeyType.Text)
-    .Key(x => x.Color, KeyType.Text)
-    .CreateAsync();
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    
+    Console.WriteLine(e);
+}
 
 app.Run();
