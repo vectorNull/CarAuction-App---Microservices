056e1bb8d7266a100f92cde1a3bc3403ee4ad688
vectorNull <jose.rodriguez76@protonmail.com>
2023-09-08 16:31:14 -0700

    frontend cleanup; nextjs initial commit

---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <div>
+      <h3 className='text-3xl font-semibold'>
+        Car Auction App
+      </h3>
+   </div>
+  )
+}
