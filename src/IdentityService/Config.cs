09b3876d14263de9c0d424ab5c7bbd95897cba48
vectorNull <jose.rodriguez76@protonmail.com>
2023-09-13 21:19:16 -0700

    Add userClaims in IdToken for NextAuth.js

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -40,7 +40,8 @@ public static class Config
                 RedirectUris = { "http://localhost:3000/api/v1/auth/callback/id-server" },
                 AllowOfflineAccess = true,
                 AllowedScopes = { "openid", "profile", "carAuctionApp" },
-                AccessTokenLifetime = 3600*24*30
+                AccessTokenLifetime = 3600*24*30,
+                AlwaysIncludeUserClaimsInIdToken = true
             }
         };
 }
