18193ed08901b413cd04611ee7ab7572d25d10da
vectorNull <jose.rodriguez76@protonmail.com>
2023-09-28 19:56:04 -0700

    routing for auction create, update, delete. Winner and seller useractions.

--- frontend/web-app/app/api/auth/[...nextauth]/route.ts
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -20,7 +20,7 @@ export const authOptions: NextAuthOptions = {
         })
     ],
     callbacks: {
-        async jwt({ token, profile, account }) {
+        async jwt({ token, profile, account, user }) {
             if (profile) {
                 token.username = profile.username
             }
--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -7,29 +7,44 @@ import { HiCog, HiUser } from 'react-icons/hi2'
 import Link from 'next/link'
 import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai'
 import { signOut } from 'next-auth/react'
+import { usePathname, useRouter } from 'next/navigation'
+import { useParamsStore } from '@/hooks/useParamsStore'
 
 type Props = {
-	user: Partial<User>
+	user: User
 }
 
-export default function UserActions({user}: Props) {
+export default function UserActions({ user }: Props) {
+	const router = useRouter();
+	const pathname = usePathname();
+	const setParams = useParamsStore(state => state.setParams);
+	console.log("user: ", user);
+
+	function setWinner() {
+		setParams({ winner: user.username, seller: undefined })
+		console.log('inside setWinner: ', user.username);
+		if (pathname !== '/') router.push('/')
+	}
+
+	function setSeller() {
+		setParams({ seller: user.username, winner: undefined })
+		console.log('inside setSeller: ', user.username);
+		if (pathname !== '/') router.push('/')
+	}
+
 	return (
 		<Dropdown
 			inline
 			label={`Welcome ${user.name}`}
 		>
-			<Dropdown.Item icon={HiUser}>
-				<Link href='/'>
-					My Auctions
-				</Link>
+			<Dropdown.Item icon={HiUser} onClick={setSeller}>
+				My Auctions
 			</Dropdown.Item>
-			<Dropdown.Item icon={AiFillTrophy}>
-				<Link href='/'>
-					Auctions Won
-				</Link>
+			<Dropdown.Item icon={AiFillTrophy} onClick={setWinner}>
+				Auctions Won
 			</Dropdown.Item>
 			<Dropdown.Item icon={AiFillCar}>
-				<Link href='/'>
+				<Link href='/auctions/create'>
 					Sell My Car
 				</Link>
 			</Dropdown.Item>
@@ -39,8 +54,8 @@ export default function UserActions({user}: Props) {
 				</Link>
 			</Dropdown.Item>
 			<Dropdown.Divider />
-			<Dropdown.Item icon={AiOutlineLogout} onClick={() => signOut({callbackUrl: '/'})}>
-					Sign Out
+			<Dropdown.Item icon={AiOutlineLogout} onClick={() => signOut({ callbackUrl: '/' })}>
+				Sign Out
 			</Dropdown.Item>
 		</Dropdown>
 	)
